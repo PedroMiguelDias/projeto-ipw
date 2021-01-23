cf0b666ff13fe61754bf61111a9fec5f9f4c22d2
Pedro Dias <pedrodias2601@gmail.com>
2021-01-23 19:46:47 +0000

    Add the ability to upload files and write news. * Update some tags that were cause errors in W3C HTML validator

--- app.js
+++ app.js
@@ -3,31 +3,58 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
 const app = express();
+const multer = require('multer');
 const PORT = process.env.PORT || 3000;
 
 let posts = [];
 let comments = [];
+let news = [];
 
+// Set the rendering engine to EJS
 app.set('view engine', 'ejs');
 
-// We use body parser to grab information from HTML form fields
+// We use body parser to grab information from HTML form fields (only text)
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __dirname + '/public/img');
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, fileName)
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Apenas ficheiros permitidos do tipo: .png, .jpg, .jpeg'));
+        }
+    }
+});
+
+
+app.get("/", function (req, res) {
     res.render("index", {
+        news: news,
         pageTitle: "Página Inicial"
     });
 });
 
-app.get('/atividade', function (req, res) {
+app.get("/atividade", function (req, res) {
     res.render("activity", {
         posts: posts,
         pageTitle: "Atividade"
     });
 });
 
-app.get('/post/:postName', function (req, res) {
+app.get("/post/:postName", function (req, res) {
     const requestedTitle = _.lowerCase(req.params.postName);
 
     posts.forEach(function (post) {
@@ -46,19 +73,33 @@ app.get('/post/:postName', function (req, res) {
     });
 });
 
-app.get('/noticias', function (req, res) {
+app.get("/noticias", function (req, res) {
     res.render("news", {
+        news: news,
         pageTitle: "Todas as Notícias"
     });
 });
 
-app.get('/noticia-detalhe', function (req, res) {
-    res.render("news-detail", {
-        pageTitle: "Notícia"
+app.get("/noticia-detalhe/:newsName", function (req, res) {
+    const requestedTitle = _.lowerCase(req.params.newsName);
+
+    news.forEach(function (newNews) {
+        const storedTitle = _.lowerCase(newNews.title);
+
+        if (storedTitle === requestedTitle) {
+            res.render("news-detail", {
+                title: newNews.title,
+                content: newNews.content,
+                date: newNews.date,
+                image: newNews.image,
+                pageTitle: newNews.name
+            });
+        }
     });
+
 });
 
-app.get('/about-us', function (req, res) {
+app.get("/about-us", function (req, res) {
     res.render("about-us", {
         pageTitle: "Sobre os Criadores"
     });
@@ -66,7 +107,13 @@ app.get('/about-us', function (req, res) {
 
 app.get("/compose", function (req, res) {
     res.render("compose", {
-        pageTitle: "Novo Post"
+        pageTitle: "Nova Publicação"
+    });
+});
+
+app.get("/compose-news", function (req, res) {
+    res.render("compose-news", {
+        pageTitle: "Nova Notícia"
     });
 });
 
@@ -88,8 +135,7 @@ app.post("/comments", function (req, res) {
 
     comments.push(comment);
 
-    res.redirect('back');
-
+    res.redirect("back");
 });
 
 app.post("/compose", function (req, res) {
@@ -105,7 +151,23 @@ app.post("/compose", function (req, res) {
 
     posts.push(post);
 
-    res.redirect("/atividade");
+    res.redirect("/post/" + post.title);
+});
+
+app.post("/compose-news", upload.single('newsImage'), function (req, res) {
+    let newsDate = getDate();
+
+    // Request the data that was inserted in the form
+    const newNews = {
+        title: req.body.newsTitle,
+        content: req.body.newsBody,
+        date: newsDate,
+        image: req.file.filename
+    };
+
+    news.push(newNews);
+
+    res.redirect('/noticia-detalhe/' + newNews.title);
 });
 
 function getDate() {
